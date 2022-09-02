609d77d7402d856fabe541bbef3ef8111cb21a3c
NicolasMerget <Nicolas.Merget@deutschebahn.com>
2022-09-02 16:37:18 +0200

    feat: improve import from zeplin

--- scripts/zeplin-styleguide.js
+++ scripts/zeplin-styleguide.js
@@ -8,23 +8,86 @@ const zeplin = new ZeplinApi(
 	})
 );
 
+const correctKey = (key) => {
+	let correctKey = key;
+	for (let i = 0; i < 20; i++) {
+		const replacementNumber = String(i).padStart(2, '0');
+		while (correctKey.includes(replacementNumber)) {
+			correctKey = correctKey.replace(`${replacementNumber}-`, '');
+		}
+	}
+	return correctKey;
+};
+
+const convertColors = (data) => {
+	const keys = Object.keys(data.colors);
+	const newColors = {};
+	keys.forEach((key) => {
+		const color = data.colors[key];
+		// TODO: Merge Similar Keys together
+		newColors[correctKey(key)] = { value: color.value };
+	});
+	data.colors = newColors;
+};
+
+const convertTextStyles = (data) => {
+	const keys = Object.keys(data.textStyles);
+
+	const newTextStyles = {};
+	keys.forEach((key) => {
+		const textStyle = data.textStyles[key];
+		delete textStyle.value.color;
+		// TODO: Merge Similar Keys together
+		const cKey = correctKey(key).replace('foundation-', '');
+		newTextStyles[cKey] = { value: textStyle.value };
+		if (cKey.includes('bold')) {
+			newTextStyles[cKey].woff = {
+				value: 'assets/fonts/dbscreensans-bold.woff'
+			};
+			newTextStyles[cKey].woff2 = {
+				value: 'assets/fonts/dbscreensans-bold.woff2'
+			};
+		} else if (textStyle.value.font.family === 'DBScreenHead') {
+			newTextStyles[cKey].woff = {
+				value: 'assets/fonts/dbscreenhead-light.woff'
+			};
+			newTextStyles[cKey].woff2 = {
+				value: 'assets/fonts/dbscreenhead-light.woff2'
+			};
+		} else {
+			newTextStyles[cKey].woff = {
+				value: 'assets/fonts/dbscreensans-regular.woff'
+			};
+			newTextStyles[cKey].woff2 = {
+				value: 'assets/fonts/dbscreensans-regular.woff2'
+			};
+		}
+	});
+	data.textStyles = newTextStyles;
+};
+
+const convertSpacings = (data) => {
+	const keys = Object.keys(data.spacing);
+	const newSpacings = {};
+	keys.forEach((key) => {
+		const spacing = data.spacing[key];
+		const cKey =
+			key.split('-').length === 3 ? key.replace('-5', '.5') : key;
+		newSpacings[cKey] = {
+			value: `${spacing.value}px`
+		};
+	});
+	data.spacing = newSpacings;
+};
+
 (async () => {
 	try {
 		const { data } = await zeplin.designTokens.getStyleguideDesignTokens(
 			'63037ab49bdcb913c9228718'
 		);
-
-		const textStyleKeys = Object.keys(data.textStyles);
-		textStyleKeys.forEach((key) => {
-			const textStyle = data.textStyles[key];
-			data.textStyles[key] = {
-				...textStyle.value,
-				// I guess we don't need this color here
-				color: {
-					value: textStyle.value.color.replace('$colors', 'colors')
-				}
-			};
-		});
+		convertColors(data);
+		convertTextStyles(data);
+		convertSpacings(data);
 
 		FS.writeFileSync('./tokens/zeplin.json', JSON.stringify(data));
 	} catch (e) {
