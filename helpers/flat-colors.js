abbbce63198a3aa4808c1c724c15ee5912b7fcfb
NicolasMerget <Nicolas.Merget@deutschebahn.com>
2022-09-26 14:21:41 +0200

    fix: issues with foreground/background

--- helpers/flat-colors.js
+++ helpers/flat-colors.js
@@ -5,7 +5,13 @@ const getOnHeader = (header) => {
 	return splitHeader.filter((part) => Number.isNaN(Number(part))).join('-');
 };
 
-const addListElement = (header, key, value) => {
+const colorStates = ['enabled', 'hover', 'pressed'];
+
+const isDefaultChild = (key) => {
+	return colorStates.includes(key) || key === 'on';
+};
+
+const addListElement = (header, value) => {
 	return `
 <!-- TODO: Make this clickable with copy to clipboard-->
 	<li>
@@ -13,14 +19,14 @@ const addListElement = (header, key, value) => {
 			<span class="sg-label" style="color: ${
 				value?.foreground || 'rgb(236, 0, 22)'
 			};">
-				<strong>${getOnHeader(header)}-${key}</strong>
+				<strong>${getOnHeader(header)}-${value.foregroundKey}</strong>
 				<br />
 				${value?.foreground || 'not available'}
 				${
 					value?.weak
-						? `<br /><br /><strong>${getOnHeader(
-								header
-						  )}-weak-${key}</strong><br />${value?.weak}`
+						? `<br /><br /><strong>${getOnHeader(header)}-weak-${
+								value.foregroundKey
+						  }</strong><br />${value?.weak}`
 						: ''
 				}
 			</span>
@@ -28,7 +34,7 @@ const addListElement = (header, key, value) => {
 		<span class="sg-label" style="color: ${
 			value?.background ? 'inherit' : 'rgb(236, 0, 22)'
 		};">
-			<strong>${header}-${key}</strong>
+			<strong>${header}-${value.backgroundKey}</strong>
 			<br />
 			${value?.background || 'not available'}
 		</span>
@@ -36,61 +42,98 @@ const addListElement = (header, key, value) => {
 	`;
 };
 
-const addList = (header, headerObject, keys) => {
-	let resultString = `<ul class="sg-colors">`;
-
-	const listElements = {};
-
-	keys.forEach((key) => {
-		if (key === 'on') {
-			const onObject = headerObject['on'];
-			Object.keys(onObject)
-				.filter((onKey) => onKey !== 'bg')
-				.forEach((onKey) => {
-					if (onKey === 'weak') {
-						const weakObject = onObject['weak'];
-						Object.keys(weakObject).forEach((weakKey) => {
-							listElements[weakKey] = {
-								...listElements[weakKey],
-								weak: weakObject[weakKey]?.value
-									? weakObject[weakKey].value
-									: 'error'
-							};
-						});
-					} else {
-						listElements[onKey] = {
-							...listElements[onKey],
-							foreground: onObject[onKey]?.value
-								? onObject[onKey].value
-								: 'error'
-						};
-					}
-				});
-		} else {
-			listElements[key] = {
-				...listElements[key],
-				background: headerObject[key]?.value
-					? headerObject[key].value
-					: 'error'
-			};
-		}
-	});
-
-	Object.keys(listElements).forEach((key) => {
-		resultString += addListElement(header, key, listElements[key]);
-	});
-
-	resultString += `</ul>`;
-	return resultString;
+const getTryoutElement = (
+	colorObj,
+	defaultBackground,
+	defaultForeground,
+	isBackground
+) => {
+	const stateElement = isBackground ? 'backgroundColor' : 'color';
+	return `<li>
+				<div class="sg-swatch"
+				onmousedown="this.style['${stateElement}']='${colorObj[colorStates[2]].value}'"
+				onmouseup="this.style['${stateElement}']='${colorObj[colorStates[1]].value}'"
+				onmouseover="this.style['${stateElement}']='${colorObj[colorStates[1]].value}'"
+				onmouseout="this.style['${stateElement}']='${colorObj[colorStates[0]].value}'"
+				style="background-color: ${
+					isBackground
+						? colorObj[colorStates[0]].value
+						: defaultBackground
+				};
+				color: ${isBackground ? defaultForeground : colorObj[colorStates[0]].value};"
+				>
+					<span class="sg-label">
+						<strong>My ${isBackground ? 'background' : 'foreground'}</strong>
+						<br />
+						<span>is interactive!</span>
+					</span>
+				</div>
+				<strong class="sg-label">Try me: ${
+					isBackground ? 'background' : 'foreground'
+				} interactive</strong>
+			</li>`;
 };
 
-const isDefaultChild = (key) => {
-	return (
-		key === 'enabled' ||
-		key === 'hover' ||
-		key === 'pressed' ||
-		key === 'on'
-	);
+const addList = (header, headerObject, keys) => {
+	let resultString = '';
+
+	let defaultForeground;
+	let defaultBackground = headerObject[colorStates[0]]?.value;
+	let onObject;
+
+	if (keys.includes('on')) {
+		onObject = headerObject['on'];
+		defaultForeground = onObject[colorStates[0]]?.value;
+	}
+
+	if (defaultBackground) {
+		resultString += `<strong>${header} background interactive</strong>`;
+		resultString += `<ul class="sg-colors">`;
+		for (let i = 0; i < 3; i++) {
+			const state = colorStates[i];
+			if (headerObject[state]) {
+				resultString += addListElement(header, {
+					backgroundKey: state,
+					background: headerObject[state].value,
+					foregroundKey: colorStates[0],
+					foreground: defaultForeground
+				});
+			}
+		}
+		resultString += getTryoutElement(
+			headerObject,
+			defaultBackground,
+			defaultForeground,
+			true
+		);
+		resultString += `</ul>`;
+	}
+
+	if (onObject) {
+		resultString += `<strong>${header} foreground interactive</strong>`;
+		resultString += `<ul class="sg-colors">`;
+		for (let i = 0; i < 3; i++) {
+			const state = colorStates[i];
+			if (onObject[state]) {
+				resultString += addListElement(header, {
+					backgroundKey: colorStates[0],
+					background: defaultBackground,
+					foregroundKey: state,
+					foreground: onObject[state].value,
+					weak: onObject.weak?.[state]?.value
+				});
+			}
+		}
+		resultString += getTryoutElement(
+			onObject,
+			defaultBackground,
+			defaultForeground,
+			false
+		);
+		resultString += `</ul>`;
+	}
+
+	return resultString;
 };
 
 module.exports = function (Handlebars) {
@@ -131,7 +174,7 @@ module.exports = function (Handlebars) {
 					const otherChildren = bgKeys.filter(
 						(key) => !isDefaultChild(key)
 					);
-					resultString += `<h3>${header}-backgrounds-only</h3>`;
+					resultString += `<h3>${header} backgrounds light (only)</h3>`;
 					resultString += addList(
 						`${header}-bg`,
 						bgObject,
