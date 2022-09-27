68567af3f9db5c719b84a2f2572b58612b653ca1
NicolasMerget <Nicolas.Merget@deutschebahn.com>
2022-09-27 08:25:51 +0200

    fix: add try catch to auto-update-pr.js

---
+++ .github/scripts/auto-update-pr.js
@@ -0,0 +1,32 @@
+module.exports = async ({ github, context }) => {
+	try {
+		const { repo, owner } = context.repo;
+		const result = await github.rest.pulls.create({
+			title: 'Auto Update from main',
+			owner,
+			repo,
+			head: '${{ inputs.head }}',
+			base: '${{ inputs.base }}',
+			body: '(o゜▽゜)o ☆ ☜(ﾟヮﾟ☜)'
+		});
+
+		await github.rest.issues.addLabels({
+			owner,
+			repo,
+			issue_number: result.data.number,
+			labels: ['automated update']
+		});
+
+		await github.pulls.createReview({
+			owner,
+			repo,
+			pull_number: result.data.number,
+			event: 'APPROVE'
+		});
+
+		return result.data.html_url;
+	} catch (e) {
+		console.error(e);
+	}
+	return false;
+};
