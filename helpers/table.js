358474ff96d593a298f768225ff9d22da253ca0f
NicolasMerget <Nicolas.Merget@deutschebahn.com>
2022-09-30 10:30:42 +0200

    feat: add spacings to patternlab

---
+++ helpers/table.js
@@ -0,0 +1,13 @@
+module.exports = function (Handlebars) {
+	Handlebars.registerHelper('table', function (context) {
+		if (context.data.root.table) {
+			const tableStringify = JSON.stringify(
+				context.data.root.table
+			).replace(/"/g, '&quot;');
+			const table = `<db-table tabledata="${tableStringify}"></db-table>`;
+
+			return new Handlebars.SafeString(table);
+		}
+		return '<span>ERROR TABLE</span>';
+	});
+};
