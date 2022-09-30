1fbfb8451ad938957fc9a9f1c046a0d39240a799
NicolasMerget <Nicolas.Merget@deutschebahn.com>
2022-09-30 14:12:44 +0200

    feat: add cleanup for gh-pages

---
+++ .github/scripts/cleanup-gh-pages.js
@@ -0,0 +1,49 @@
+/*
+ * Fetches all branches and deletes all review-branches in github pages
+ */
+const FS = require('fs');
+
+const TAG = 'cleanup-gh-pages:';
+
+const removeOldFromPath = (isTag, data) => {
+	const path = `public/${isTag ? 'version' : 'review'}`;
+	if (FS.existsSync(path) && data?.length > 0) {
+		const dirsToDelete = FS.readdirSync(path).filter(
+			(file) => !data.find((branch) => branch.name === file)
+		);
+		if (dirsToDelete?.length > 0) {
+			console.log(
+				TAG,
+				`Start removing ${isTag ? 'tags' : 'branches'} from gh-pages`
+			);
+			console.log(TAG, dirsToDelete);
+			dirsToDelete.forEach((dir) => {
+				FS.rmSync(`${path}/${dir}`, {
+					recursive: true,
+					force: true
+				});
+				console.log(TAG, `deleted  ${isTag ? 'tag' : 'branch'} ${dir}`);
+			});
+		} else {
+			console.log(
+				TAG,
+				`All ${isTag ? 'tags' : 'branches'} are up to date`
+			);
+		}
+	}
+};
+
+module.exports = async ({ github, context }) => {
+	const { repo, owner } = context.repo;
+	const branches = await github.rest.repos.listBranches({
+		owner,
+		repo
+	});
+	const tags = await github.rest.repos.listTags({
+		owner,
+		repo
+	});
+
+	removeOldFromPath(false, branches);
+	removeOldFromPath(true, tags);
+};
