b6dd19b6283ae36b9737d0242a688507639a8bf8
Maximilian Franzke <Maximilian.Franzke@deutschebahn.com>
2025-05-28 18:42:06 +0200

    refactor: migrated config file
    https://eslint.org/docs/latest/use/configure/migration-guide#migrate-your-config-file

--- .eslintrc.js
+++
@@ -1,6 +0,0 @@
-// SPDX-FileCopyrightText: 2025 DB Systel GmbH
-//
-// SPDX-License-Identifier: Apache-2.0
-
-// eslint-disable-next-line unicorn/prefer-module
-module.exports = { env: { browser: true, es2021: true, node: true } };
